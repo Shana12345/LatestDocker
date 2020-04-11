2acbf8e1d279e8833a1d75106738b2b442a8fc7b
Shana12345 <shana_charlery@msn.com>
2020-04-11 22:46:54 +0000

    Forever commited!

--- tests/testing.py
+++ tests/testing.py
@@ -5,9 +5,6 @@ def test_home():
     r = http.request('GET', "http://35.222.72.134/")
     assert 200 == r.status
     
-def test_negative():
-    http = urllib3.PoolManager()
-    r = http.request('GET', "http://35.222.72.134/daemon")
-    assert r.status == 404
+
 
 
